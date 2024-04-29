f682d2ee4037119b00b89f0da72fff09a71e9fe6
MustafaAykut77 <168454893+MustafaAykut77@users.noreply.github.com>
2024-04-29 18:22:44 +0300

    Update Arkaplan.js

--- classes/Arkaplan.js
+++ classes/Arkaplan.js
@@ -1,16 +1,19 @@
+//ARKAPLAN SINIFI
 class Arkaplan {
+    //constructor
     constructor({pozisyon, resimYolu}){
         this.pozisyon = pozisyon;
         this.image = new Image();
         this.image.src = resimYolu;
     }
+    //cizme fonksiyonu
     ciz(){
         if(!this.image){
             return;
         }
         c.drawImage(this.image, this.pozisyon.x, this.pozisyon.y)
     }
-
+    //guncelleme fonksiyonu
     guncelle(){
         this.ciz();
     }
