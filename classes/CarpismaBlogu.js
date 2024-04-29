a22b12eaf2916535186877c53c197ab44ad7a17a
MustafaAykut77 <168454893+MustafaAykut77@users.noreply.github.com>
2024-04-29 18:24:02 +0300

    Update CarpismaBlogu.js

--- classes/CarpismaBlogu.js
+++ classes/CarpismaBlogu.js
@@ -1,17 +1,18 @@
+//CARPISMA BLOGU SINIFI
 class CarpismaBlogu {
+    //constructor
     constructor({pozisyon}){
         this.pozisyon = pozisyon;
         this.width = 32;
         this.height = 32;
         this.image = new Image();
-        this.image.src = "./img/cobblestone.png"
-        
+        this.image.src = "./img/cobblestone.png";
     }   
-
+    //cizme fonksiyonu
     ciz(){
         c.drawImage(this.image, this.pozisyon.x, this.pozisyon.y);
     }
-
+    //guncelleme fonksiyonu
     guncelle(){
         this.ciz();
     }
